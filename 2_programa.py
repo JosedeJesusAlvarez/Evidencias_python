301beeb1f6b6e8781613ae73bd76a0091c85c5db
Jose de Jesus Alvarez <jose.alvarez4196@alumnos.udg.mx>
2023-06-02 15:46:50 -0600

    Termine la secion 3

--- 2_programa.py
+++ 2_programa.py
@@ -1,8 +1,21 @@
-precioDeLaManzana:int = 10
-cantidadDeManzanas: int = 5
-print ("Vas a pagar")
-print(precioDeLaManzana*cantidadDeManzanas)
+#precioDeLaManzana:int = 10
+#cantidadDeManzanas: int = 5
+#print ("Vas a pagar")
+#print(precioDeLaManzana*cantidadDeManzanas)
 """ 
-    Comentario multilineas
+    Comentario multilineas 
     
 """
+#concatenacion
+precioDeManzana:int=int(input("Precio de la manzana: "))
+cantidadDeManzanas:int=int(input("Cantidad de manzanas: "))
+print("vas a pagar")
+print("las manzanas estan en : " + str(precioDeManzana))
+print("las manzanas estan en: ", precioDeManzana)
+print(f"las manzanas estan en: {precioDeManzana} y fueron: {cantidadDeManzanas}")
+print("las manzanas estan en : ")
+print(precioDeManzana)
+print("y fueron")
+print(cantidadDeManzanas)
+print("total a pagar")
+print(precioDeManzana*cantidadDeManzanas)
