1d83b0d997ea77fac7e99ebd9376123937265123
Jose de Jesus Alvarez <jose.alvarez4196@alumnos.udg.mx>
2023-06-07 14:26:50 -0600

    Agregue la parte del IF

--- 2_programa.py
+++ 2_programa.py
@@ -7,15 +7,24 @@
     
 """
 #concatenacion
-precioDeManzana:int=int(input("Precio de la manzana: "))
-cantidadDeManzanas:int=int(input("Cantidad de manzanas: "))
-print("vas a pagar")
-print("las manzanas estan en : " + str(precioDeManzana))
-print("las manzanas estan en: ", precioDeManzana)
+#precioDeManzana:int=int(input("Precio de la manzana: "))
+#cantidadDeManzanas:int=int(input("Cantidad de manzanas: "))
+#print("vas a pagar")
+#print("las manzanas estan en : " + str(precioDeManzana))
+"""print("las manzanas estan en: ", precioDeManzana)
 print(f"las manzanas estan en: {precioDeManzana} y fueron: {cantidadDeManzanas}")
 print("las manzanas estan en : ")
 print(precioDeManzana)
 print("y fueron")
 print(cantidadDeManzanas)
 print("total a pagar")
-print(precioDeManzana*cantidadDeManzanas)
+print(precioDeManzana*cantidadDeManzanas)"""
+manzanasCompradas:int=int(input("Cuantas manzanas compro: "))
+precioManzanas:int=float(input("Precio de la manzana: "))
+if manzanasCompradas >=10:
+    print("!Tienes descuento¡")
+    print((precioManzanas*manzanasCompradas)*.10)
+    descuento=((precioManzanas*manzanasCompradas)*.10)
+print(f"Compraste: {manzanasCompradas} y las manzanas cuestan: {precioManzanas}")
+print("Pagas")
+print((precioManzanas*manzanasCompradas)-descuento)
---
+++ 3_programa_if.py
@@ -0,0 +1,14 @@
+#edad= 18
+#traes50Pesos= True
+#dia="lunes"
+#if (traes50Pesos and dia == "miercoles") or edad>=18 :3    print("compra una cervecita")
+#print(edad>=18)
+#print("Adios")
+
+#edad:int=int(input("Introduce tu edad: "))
+#mes=input("En que mes estas: ")
+#tengoMasdeMilpesos:int=int(input("Cuanto dinero tienes: "))
+#if tengoMasdeMilpesos >=1000 and edad>=18 and mes.lower=="julio":
+    #print("Vas a la playa")
+#print("Adios.")
+# El .lower hace todas las variables minusculas.
